c605c9622c355270237a7586a1418cad0347f1e4
Roland Vachter <roland.vachter@iquestgroup.com>
2016-04-29 17:23:39 +0300

    Duplicate path declaration fixed.

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const fs = require('fs');
 const express = require('express');
-const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
