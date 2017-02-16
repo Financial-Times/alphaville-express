1904d26da53e0281f3eae6869af0a4315ab6c940
Roland Vachter <roland.vachter@iquestgroup.com>
2017-02-16 12:15:11 +0200

    Add copyrightYear default variable for views.

--- main.js
+++ main.js
@@ -55,7 +55,8 @@ module.exports = function (options) {
 		basePath: '/' + options.appBasePath,
 		isTest: environment === 'test' ? true : false,
 		isProd: environment === 'prod' ? true : false,
-		polyfillServiceUrl: '//cdn.polyfill.io/v2/polyfill.min.js?features=default,fetch|gated&excludes=Symbol,Symbol.iterator,Symbol.species,Map,Set'
+		polyfillServiceUrl: '//cdn.polyfill.io/v2/polyfill.min.js?features=default,fetch|gated&excludes=Symbol,Symbol.iterator,Symbol.species,Map,Set',
+		copyrightYear: new Date().getFullYear()
 	};
 
 	app.engine('handlebars', alphavilleHbs.engine);
